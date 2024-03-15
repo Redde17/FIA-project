908a9bd6b75c7423657e1449a111ad86ac6643f3
Redde17 <redsmile797@gmail.com>
2024-03-16 00:02:12 +0100

    Added some keyboard interactions for handling program flow.

--- MazeAI/Main.cpp
+++ MazeAI/Main.cpp
@@ -13,44 +13,24 @@ using std::chrono::high_resolution_clock;
 #define MAP_Y 100
 #define GRID_SIZE 10
 
+//int runs = 0;
+duration<double, std::milli> mazeGenerationTime = mazeGenerationTime.zero();
+duration<double, std::milli> mazePathFindingTime = mazePathFindingTime.zero();
+
+void generateMaze(GameHandler& GH);
+void solveMaze(GameHandler& GH);
+
 int main() {
     srand((unsigned)time(NULL));
 	GameHandler GH(MAP_X, MAP_Y, GRID_SIZE);
 
-    int runs = 0;
-    duration<double, std::milli> mazeGenerationTime = mazeGenerationTime.zero();
-    duration<double, std::milli> mazePathFindingTime = mazePathFindingTime.zero();
+    //std::cout << "Avg time for [" << runs << "] runs" << std::endl;
+    //std::cout << "Avg generation time: " << mazeGenerationTime.count() / runs << std::endl;
+    //std::cout << "Avg pathFinding time: " << mazePathFindingTime.count() / runs << std::endl;
 
     std::cout << "Program start" << std::endl;
-
-    auto t1 = high_resolution_clock::now();
-    GH.generateNewMaze();
-    auto t2 = high_resolution_clock::now();
-    duration<double, std::milli> ms_double = t2 - t1;
-    mazeGenerationTime += ms_double;
-
-    std::cout << "Maze generation time: " << ms_double.count() << "ms\n";
+    generateMaze(GH);
     GH.drawMaze();
-
-    t1 = high_resolution_clock::now();
-    GH.solveMaze();
-    t2 = high_resolution_clock::now();
-    ms_double = t2 - t1;
-    mazePathFindingTime += ms_double;
-
-    std::cout << "Path generation time: " << ms_double.count() << "ms\n";
-
-    t1 = high_resolution_clock::now();
-    GH.drawMaze();
-    t2 = high_resolution_clock::now();
-    ms_double = t2 - t1;
-
-    std::cout << "Draw time: " << ms_double.count() << "ms\n";
-    runs++;
-
-    std::cout << "Avg time for [" << runs << "] runs" << std::endl;
-    std::cout << "Avg generation time: " << mazeGenerationTime.count() / runs << std::endl;
-    std::cout << "Avg pathFinding time: " << mazePathFindingTime.count() / runs << std::endl;
 
 	while (GH.window->isOpen())
 	{
@@ -60,10 +40,51 @@ int main() {
             case sf::Event::Closed:
                 GH.window->close();
                 break;
+            case sf::Event::KeyPressed:
+                switch (event.key.scancode)
+                {
+                case sf::Keyboard::Scan::Space:
+                    generateMaze(GH);
+                    GH.drawMaze();
+
+                    solveMaze(GH);
+                    GH.drawMaze();
+                    //runs++;
+                    break;
+                case sf::Keyboard::Scan::G:
+                    generateMaze(GH);
+                    GH.drawMaze();
+                    break;
+                case sf::Keyboard::Scan::S:
+                    solveMaze(GH);
+                    GH.drawMaze();
+                    break;
+                default:
+                    break;
+                }
             default:
                 break;
             }
         }
     }
 	return 0;
+}
+
+void generateMaze(GameHandler& GH) {
+    auto t1 = high_resolution_clock::now();
+    GH.generateNewMaze();
+    auto t2 = high_resolution_clock::now();
+    duration<double, std::milli> ms_double = t2 - t1;
+    mazeGenerationTime += ms_double;
+    std::cout << "Maze generation time: " << ms_double.count() << "ms\n";
+}
+
+void solveMaze(GameHandler& GH) {
+    auto t1 = high_resolution_clock::now();
+    GH.solveMaze();
+    auto t2 = high_resolution_clock::now();
+    duration<double, std::milli> ms_double = t2 - t1;
+    mazePathFindingTime += ms_double;
+    std::cout << "Path generation time: " << ms_double.count() << "ms\n\n";
+    
 }
